124af3dfebf989879b5c93eb51522fdb06a619b9
adamyi515 <adamyi515@gmail.com>
2023-09-02 16:15:26 -0500

    Setup connection to our MONGO DB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
 
+// Connection to DB.
+import connectDB from './config/db.js';
+connectDB();
 
 
 const PORT = 5000;
